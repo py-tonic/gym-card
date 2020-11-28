2ec6f066196c31da19031395bde91edeefb51d3b
devcat <jaeheon.dev@gmail.com>
2020-11-28 19:42:51 +0900

    update SpiderEnv class and docstring

--- env.py
+++ env.py
@@ -1,5 +1,15 @@
 from collections import namedtuple
 
+'''
+num of cards 13x8 = 104
+columns = 10
+get cards = 5xcolumns = 50
+init cards = 54
+random drow 'n' rows
+simultaneously move if num is continuous
+Draw one card in n columns
+Erase when Ace to King is completed
+'''
 
 class SpiderEnv:
     '''
@@ -9,13 +19,16 @@ class SpiderEnv:
     Max Reward = 1254
     min move = 46
     '''
-    def __init__(self):
+    def __init__(self, mode='easy'):
+        if mode is 'easy':
+            self.cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']*8
+            self.move = 0
+        '''
         self.Card = namedtuple('Card', ['value','suit'])
         self.suits = ['hearts','diamonds','spades','clubs']
         self.cards = [self.Card(value, suit) for value in range(1,14) for suit in self.suits]
+        '''
 
 
-
-env = Spade()
-print(env.cards[0])
-print(len(env.cards))
+env = SpiderEnv()
+print(env.cards)
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+'''
+Q-Learnign version
+DQN version
+'''
 if __name__ =='__main__':
     print('welcome to Casino')
